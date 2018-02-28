16e7345ed9adf425c7297b7be6e1532564544ca2
charleneeshong@berkeley.edu <charleneeshong@berkeley.edu>
2018-02-28 11:17:10 -0800

    last few lab3 files

--- lab3/lab3.m
+++ lab3/lab3.m
@@ -1,42 +1,37 @@
-%%ME134 - Lab 3
-%Jenny Wong, Charlene Shong, Trey Fortmuller
+%% Lab 3
 
-%% Pre-lab 4.3 MATLAB Step Response
-% finding an appropriate gain based on the given design requirements
-% for a proportional controller for a cart on a rail
+%% Pre-lab MATLAB Step Input
+%Charlene Shong
 
-% define constants for the plant dynamics
-m_c = 0.94; % [kg]
-r = 6.36e-3; % [m]
-R_m = 2.6; % [ohms]
-K_t = 7.67e-3; % [Nm/A]
-K_m = 7.67e-3; % [Vs/rad]
-K_g = 3.72; % gear ratio
-J_m = 3.9e-7; % [kgm^2]
+% System Parameters
+mc = 0.94; %kg
+r = 6.36e-3; %m
+Rm = 2.6; %ohm
+Kt = 7.67e-3; %Nm/A
+Km = 7.67e-3; %Vs/rad
+Kg = 3.71;
+Jm = 3.9e-7; %kg*m^2
 
-% the common denominator in some of the state space matrix components
-den = J_m * K_g^2 * R_m + r^2 * R_m * m_c;
-
-% create the state space model for the plant
-A = [0 1; 0 (K_t* K_m * K_g^2) / den];
-B = [0; (r * K_t * K_g) / den];
-C = [1 0];
-D = 0;
-
-sys = ss(A, B, C, D);
-
-% gain K in the simulink model
+% Set Gain
 K = 15;
 
-% run the simulink model of the cart system and open the output
-sim('prelab3.slx');
-open('prelab3.slx');
+% Simulink Model
+sim('prelab3.mdl');
+open('prelab3.mdl');
+plot(simout);xlabel('Time (ms)');ylabel('Response');
+title('Step Response of Cart System');
+stepinfo(simout.data,simout.time)
+close('prelab3.mdl');
 
-% plot the step response of the closed loop system
-plot(response);
-title('Cart Controller Step Response');
-xlabel('Time (seconds)');
-ylabel('Position (meters)');
+%% 5.3
+r = 0.01482975; % pinion radius
+res = 4096; % counts/rev
+res_t = res/(2*pi*r); % theoretical encoder resolution
+res_a = 44000; % actual encoder resolution from calculations
+err = abs(res_t - res_a)/res_t*100; % percent error
 
-% get step response data
-disp(stepinfo(sys));
+disp(['actual encoder resolution:',num2str(res_a)])
+disp(['percent error from theoretical:',num2str(err)])
+
+%% 5.4
+Kp = 60;
