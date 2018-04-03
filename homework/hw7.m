b13d74a4db1458bbd08710efd72315d3a9a9c6ae
Trey Fortmuller <tfortmuller@mac.com>
2018-04-03 01:32:46 -0700

    homework 7 completed

--- homework/hw7.m
+++ homework/hw7.m
@@ -41,7 +41,7 @@ f1 = figure;
 subplot(2, 2, 1);
 rlocus(G) % the root locus of the closed loop tf, to find he gain at zeta
 sgrid(0.456, []);
-damp(G) % get the open loop pole locations
+damp(G) % get the uncompensated pole locations
 % controlSystemDesigner(G)
 
 K = 1160; % the gain for zeta = 0.456
@@ -69,8 +69,6 @@ subplot(2, 2, 3);
 rlocus(T); % root locus of the compensated system
 sgrid(0.456, []);
 
-K_comp = 720;
-
 T_cl = feedback(2150*T, 1); % the compensated, closed loop transfer function
 subplot(2, 2, 4); % plotting the step response of the compensated system
 step(T_cl); % print the step info of the uncompensated system
@@ -87,12 +85,62 @@ step(T_cl);
 close all;
 
 G = tf(1, conv([1 0], conv([1 6], [1, 10]))); % the uncompensated ol tf
-rlocus(G);
-sgrid(0.456, []);
+damp_ratio = 0.456; % the desired damping ratio of the system
+
+f1 = figure;
+subplot(2, 2, 1);
+rlocus(G) % plot the root locus of the uncompensated system
+sgrid(damp_ratio, []);
 K = 190;
-a
-G_cl = feedback(K*G, 1)
+
+G_cl = feedback(K*G, 1);
 stepinfo(G_cl)
+subplot(2, 2, 2);
 step(G_cl)
 
-pidtool(G)
+% the real part of the location of the compensated dominant poles
+uncomp_tp = 0.9968; % the uncompensated system's peak time
+sigma = pi / (uncomp_tp * damp_ratio * sqrt(1 - damp_ratio^2));
+
+% get the uncompensated pole locations
+% damp(G);
+
+% the angular contribution of the compensator's zero to the poles
+% get the angle between the ol poles and the desired cl pole
+positive_real = [1, 0, 0]; % +real axis, with dim of 3 for cross product
+des_pole1 = [-7.766, 15.1570, 0]; % vector from the 1st ol pole to desired
+des_pole2 = [-7.766 + 6, 15.1570, 0];
+des_pole3 = [-7.766 + 10, 15.1570, 0];
+
+theta1 = atan2d(norm(cross(positive_real,des_pole1)),dot(positive_real,des_pole1));
+theta2 = atan2d(norm(cross(positive_real,des_pole2)),dot(positive_real,des_pole2));
+theta3 = atan2d(norm(cross(positive_real,des_pole3)),dot(positive_real,des_pole3));
+
+sum = theta1 + theta2 + theta3; % sum of the pole angles
+
+theta_z = sum - 180; % the angular contribution required for the compensated pole
+
+T = tf([1, 0.5720], [1, 16, 60, 0]); % the compensated open loop tf
+subplot(2, 2, 3);
+rlocus(T); % root locus of the compensated system
+sgrid(0.456, []);
+
+K_comp = 245; % the PD compensator's tuning gain
+
+T_cl = feedback(K_comp*T, 1); % the compensated, closed loop transfer function
+subplot(2, 2, 4); % plotting the step response of the compensated system
+step(T_cl); % print the step info of the uncompensated system
+stepinfo(T_cl)
+
+% plotting the step response of the uncompensated and compensated closed
+% loop systems superimposed
+f2 = figure;
+hold on;
+step(G_cl);
+step(T_cl);
+
+
+
+
+
+
--- lab5/Lab5bPre.m
+++ lab5/Lab5bPre.m
@@ -1,9 +1,9 @@
 %% Constants of the controller
-k_i = 0.114;
-k_x = 30.0676;
-a = 843;
-% k_c = 2*k_x/a/2/k_i;
-k_c = 4;
+k_i = 0.1962;
+k_x = 17.6580;
+a = 900;
+k_c = 0.5556;
+% k_c = 4;
 k_a = 2;
 m = 0.0162;
 dc = 2; % DC gain of 2
@@ -11,14 +11,14 @@ z_c = 1.5;
 p_c = z_c * 20; % pole to zero ratio of 20
 
 % Transfer function for plant
-% s = tf('s');
-% num = a * k_a * k_i;
-% den = m * (s^2 - k_x/m);
-% G = num/den;
-% fig1 = figure(1);
-% rlocus(G);
-% fig2 = figure(2);
-% step(G)
+s = tf('s');
+num = a * k_a * k_i;
+den = m * (s^2 - k_x/m);
+G = num/den;
+fig1 = figure(1);
+rlocus(G);
+fig2 = figure(2);
+step(G)
 
 % Transfer function for plant and controller
 G_c = k_c * (1 + s/z_c) / (1 + s/p_c);
